881253b3d976852e9e1230592d58bfa50829a55f
LorranKaique <148860537+LorranKaique@users.noreply.github.com>
2025-03-16 22:10:36 -0400

    correcao bugs
    correcao de destino após public. A as imagens após enviadas para pasta public estão com algum erro, e não são lidas.

--- ComputacaoESociedade/blog/gulpfile.js
+++ ComputacaoESociedade/blog/gulpfile.js
@@ -9,18 +9,26 @@ function styles() {
 }
 function images() {
     return gulp.src('./src/images/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./public/images'));
+    .pipe(gulp.dest('./public/images'))
+    .on('error', console.error);
 }
 
 function html() {
     return gulp.src('./src/*.html')
-    .pipe(gulp.dest('./public'));
+    .pipe(gulp.dest('./public'))
+    .on('error', console.error);
 }
 
-exports.default = gulp.parallel(styles, images, html);
+function js() {
+    return gulp.src('./src/js/*.js')
+    .pipe(gulp.dest('./public/js'))
+    .on('error', console.error);
+}
+
+exports.default = gulp.parallel(styles, images, html, js);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
     gulp.watch('./src/*.html', gulp.parallel(html));
+    gulp.watch('.src/js/*.js', gulp.parallel(js));
 }
